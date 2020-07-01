bb058d87a099b9e6aa971750d34cd40cde360db1
1653005 <1653005@student.hcmus.edu.vn>
2020-07-01 17:10:42 +0700

    [ANDROID] save new login information

--- app/src/main/java/com/vtb/parkingmap/database/SaigonParkingDatabase.java
+++ app/src/main/java/com/vtb/parkingmap/database/SaigonParkingDatabase.java
@@ -8,10 +8,21 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Getter;
+
 @SuppressLint("all")
 @SuppressWarnings("all")
 public final class SaigonParkingDatabase extends SQLiteOpenHelper {
 
+    private static final String USERNAME_KEY = "username";
+    private static final String ACCESS_TOKEN_KEY = "access_token";
+    private static final String REFRESH_TOKEN_KEY = "refresh_token";
+
+    @Getter
+    private Map<String, String> keyValueMap;
 
     public SaigonParkingDatabase(@Nullable Context context,
                                  @Nullable String name,
@@ -28,7 +39,7 @@ public final class SaigonParkingDatabase extends SQLiteOpenHelper {
 
     //truy vấn có trả kết quả: SELECT
     public String getData(String sql) {
-        Cursor cursor = getWritableDatabase().rawQuery(sql, null);
+        Cursor cursor = getReadableDatabase().rawQuery(sql, null);
         cursor.moveToFirst();
         String data = cursor.getString(0);
         cursor.close();
@@ -55,7 +66,51 @@ public final class SaigonParkingDatabase extends SQLiteOpenHelper {
         Log.d("BachMap", String.format("Authentication information:%nUsername: %s%nAccessToken: %s%nRefreshToken: %s%n",
                 username, accessToken, refreshToken));
 
-        /* Function Body will be writen by Nach Map Dit Thui */
+        /* Function Body will be writen by Bach Map Dit Thui */
+        int nRow = Integer.valueOf(getData("SELECT COUNT(SP_KEY) FROM SAIGON_PARKING_TABLE"));
+        Log.d("BachMap", String.format("Number of rows currently in table: %d rows", nRow));
 
+        if (nRow == 3) { /* UPDATE ROWS */
+            updateRow(USERNAME_KEY, username);
+            updateRow(ACCESS_TOKEN_KEY, accessToken);
+            updateRow(REFRESH_TOKEN_KEY, refreshToken);
+
+        } else { /* INSERT ROWS */
+            insertRow(USERNAME_KEY, username);
+            insertRow(ACCESS_TOKEN_KEY, accessToken);
+            insertRow(REFRESH_TOKEN_KEY, refreshToken);
+        }
+
+        keyValueMap = getAllRows();
+        keyValueMap.forEach((key, value) ->
+                Log.d("BachMap", String.format("Key: %s, Value: %s", key, value)));
+    }
+
+    private void updateRow(String key, String value) {
+        queryData(String
+                .format("UPDATE SAIGON_PARKING_TABLE SET SP_VALUE= '%s' WHERE SP_KEY= '%s'", value, key));
+    }
+
+    private void insertRow(String key, String value) {
+        queryData(String
+                .format("INSERT INTO SAIGON_PARKING_TABLE VALUES('%s', '%s')", key, value));
+    }
+
+    private void deleteRow(String key) {
+        queryData(String
+                .format("DELETE FROM SAIGON_PARKING_TABLE WHERE SP_KEY= '%s'", key));
+    }
+
+    private Map<String, String> getAllRows() {
+        Cursor cursor = getWritableDatabase()
+                .rawQuery("SELECT * FROM SAIGON_PARKING_TABLE", null);
+
+        Map<String, String> keyValueHashMap = new HashMap<>();
+        while (cursor.moveToNext()) {
+            String key = cursor.getString(0);
+            String value = cursor.getString(1);
+            keyValueHashMap.put(key, value);
+        }
+        return keyValueHashMap;
     }
 }
