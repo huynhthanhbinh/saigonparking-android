ff3aec7246ac1cad6120f6ba544ed7ab8b2830ae
1653005 <1653005@student.hcmus.edu.vn>
2020-07-01 11:13:28 +0700

    Revert "Base"
    This reverts commit eb7c7925183d4dbdb1396f523c8ba1c74ff92249.

--- app/src/main/java/com/vtb/parkingmap/directionhelpers/TaskLoadedCallback.java
+++
@@ -1,9 +0,0 @@
-package com.vtb.parkingmap.directionhelpers;
-
-/**
- * Created by đặng đức tài on 28/22/2020.
- */
-
-public interface TaskLoadedCallback {
-    void onTaskDone(Object... values);
-}
